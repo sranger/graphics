c5ee335eb0cdb883ec38da6a158879f7eceba82e
Stephen <stephen.w.ranger@gmail.com>
2016-08-14 16:55:21 +0000

    fixed some small issues with lod on EllipticalSegment and modified Timings toString

--- src/java/com/stephenwranger/graphics/utils/Timings.java
+++ src/java/com/stephenwranger/graphics/utils/Timings.java
@@ -6,74 +6,75 @@ import java.util.Map.Entry;
 
 public class Timings {
    private final Map<String, Timing> timings = new HashMap<>();
-   private final int historySize;
-   
+   private final int                 historySize;
+
    public Timings(final int historySize) {
       this.historySize = historySize;
    }
-   
+
+   public void end(final String key) {
+      final Timing timing = this.timings.get(key);
+
+      if (timing == null) {
+         throw new RuntimeException("Cannot end a timing without first starting it. Key not found: " + key);
+      }
+
+      timing.end();
+   }
+
+   public double getAverage(final String key) {
+      final Timing timing = this.timings.get(key);
+
+      if (timing == null) {
+         throw new RuntimeException("Cannot access a timing ; key not found: " + key);
+      }
+
+      return timing.getAverageTiming();
+   }
+
+   public long getMax(final String key) {
+      final Timing timing = this.timings.get(key);
+
+      if (timing == null) {
+         throw new RuntimeException("Cannot access a timing ; key not found: " + key);
+      }
+
+      return timing.getMaxTiming();
+   }
+
+   public long getMin(final String key) {
+      final Timing timing = this.timings.get(key);
+
+      if (timing == null) {
+         throw new RuntimeException("Cannot access a timing ; key not found: " + key);
+      }
+
+      return timing.getMinTiming();
+   }
+
    public void start(final String key) {
       Timing timing = this.timings.get(key);
-      
-      if(timing == null) {
+
+      if (timing == null) {
          timing = new Timing(this.historySize);
          this.timings.put(key, timing);
       }
-      
+
       timing.start();
    }
-   
-   public void end(final String key) {
-      final Timing timing = this.timings.get(key);
-      
-      if(timing == null) {
-         throw new RuntimeException("Cannot end a timing without first starting it. Key not found: " + key);
-      }
-      
-      timing.end();
-   }
-   
-   public long getMin(final String key) {
-      final Timing timing = this.timings.get(key);
-      
-      if(timing == null) {
-         throw new RuntimeException("Cannot access a timing ; key not found: " + key);
-      }
-      
-      return timing.getMinTiming();
-   }
-   
-   public long getMax(final String key) {
-      final Timing timing = this.timings.get(key);
-      
-      if(timing == null) {
-         throw new RuntimeException("Cannot access a timing ; key not found: " + key);
-      }
-      
-      return timing.getMaxTiming();
-   }
-   
-   public double getAverage(final String key) {
-      final Timing timing = this.timings.get(key);
-      
-      if(timing == null) {
-         throw new RuntimeException("Cannot access a timing ; key not found: " + key);
-      }
-      
-      return timing.getAverageTiming();
-   }
-   
+
    @Override
    public String toString() {
       final StringBuilder sb = new StringBuilder();
-      
-      for(final Entry<String, Timing> entry : this.timings.entrySet()) {
+
+      for (final Entry<String, Timing> entry : this.timings.entrySet()) {
          final Timing timing = entry.getValue();
-         sb.append(entry.getKey()).append(": ").append(TimeUtils.formatNanoseconds((long) timing.getAverageTiming())).append("(")
-            .append(TimeUtils.formatNanoseconds(timing.getMinTiming())).append(", ")
-            .append(TimeUtils.formatNanoseconds(timing.getMaxTiming())).append(")\n");
+         sb.append(entry.getKey()).append("\n");
+         sb.append("\tavg: ").append(TimeUtils.formatNanoseconds((long) timing.getAverageTiming())).append("\n");
+         sb.append("\tmin: ").append(TimeUtils.formatNanoseconds(timing.getMinTiming())).append("\n");
+         sb.append("\tmax: ").append(TimeUtils.formatNanoseconds(timing.getMaxTiming())).append("\n");
       }
-      
+
       return sb.toString();
    }
 }
